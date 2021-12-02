bb16e018a8025f4e571744f0fb2989f032fba222
junyuk <hellojun98@naver.com>
2021-12-02 17:15:35 +0900

    비주얼 베너 작업 중

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.visual').bxSlider({
+        mode: 'horizontal',
+        moveSlides: 1,
+        slideMargin: 10,
+        infiniteLoop: true,
+        slideWidth: 1920,
+        minSlides: 1,
+        maxSlides: 1,
+        speed: 2000,//이동속도
+    });
+    //비주얼배너 동적요소
+});
